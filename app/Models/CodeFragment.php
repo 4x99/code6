60a08ce10588da5d248b6fb8b71b0cf77338fc9a
JKeita <zsdxl4455@gmail.com>
2020-04-03 10:04:55 +0800

    其他：代码片段模型名调整

---
+++ app/Models/CodeFragment.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CodeFragment extends Model
+{
+    const UPDATED_AT = null;
+    protected $table = 'code_fragment';
+    protected $fillable = ['sha', 'content'];
+}
