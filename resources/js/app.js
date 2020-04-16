992d080787bccfb9cc2486b9a89c1c18c8706c0b
YoonPer <yoonper@qq.com>
2020-04-16 17:44:17 +0800

    特性：前端初始化
    - vue
    - vue-router
    - ant design vue
    - webpack.mix.js 配置

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,21 @@
 require('./bootstrap');
+
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import App from '../views/app.vue'
+
+Vue.use(VueRouter);
+Vue.config.productionTip = false;
+
+const routes = [];
+
+const router = new VueRouter({
+    mode: 'history',
+    routes
+});
+
+new Vue({
+    el: '#app',
+    router,
+    render: h => h(App)
+});
