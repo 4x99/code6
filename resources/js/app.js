c7b8376c77adc955dcabb81b7cd75952613edc11
YoonPer <yoonper@qq.com>
2020-04-17 10:08:43 +0800

    重构：前端脚手架标准化
    - vue-router 单独拆分
    - .vue 文件迁移至 resources/js 目录

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,18 +1,10 @@
 require('./bootstrap');
 
 import Vue from 'vue'
-import VueRouter from 'vue-router'
-import App from '../views/app.vue'
+import App from './views/App.vue'
+import router from './router'
 
-Vue.use(VueRouter);
 Vue.config.productionTip = false;
-
-const routes = [];
-
-const router = new VueRouter({
-    mode: 'history',
-    routes
-});
 
 new Vue({
     el: '#app',
