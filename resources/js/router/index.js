6cfde2c535ff8093b4f4314420841c454e04301c
huangyongxin92 <361661698@qq.com>
2020-04-20 11:24:11 +0800

    特性：前端布局构建
    - 首页模板以及导航页构建
    - 主模块路由配置(Home页、扫描结果页、任务配置页、扫描白名单页、令牌配置页)

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -1,9 +1,43 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import Layout from '../views/layout/Index';
+import Home from '../views/home/Index';
+import CodeLeak from '../views/codeLeak/Index';
+import ConfigToken from '../views/configToken/Index';
+import ConfigJob from '../views/configJob/Index';
+import ConfigWhitelist from '../views/configWhitelist/Index';
 
 Vue.use(VueRouter);
 
-const routes = [];
+const routes = [
+    {
+        path: '/',
+        component: Layout,
+        redirect: '/home',
+        children: [
+            {
+                path: 'home',
+                component: Home,
+            },
+            {
+                path: 'codeLeak',
+                component: CodeLeak,
+            },
+            {
+                path: 'configToken',
+                component: ConfigToken,
+            },
+            {
+                path: 'configJob',
+                component: ConfigJob,
+            },
+            {
+                path: 'configWhitelist',
+                component: ConfigWhitelist,
+            }
+        ],
+    }
+];
 
 const router = new VueRouter({
     mode: 'history',
