22c1f20b83ac5bfb9500f2f374f9193584dbaca1
yoonper <yoonper@qq.com>
2020-04-30 16:06:58 +0800

    其他：简化 tool.ajax 代码

--- public/js/script.js
+++ public/js/script.js
@@ -18,9 +18,10 @@ var tool = tool || {};
 
 /**
  * 消息提示
+ *
  * @param text 消息内容
- * 可选 type 消息类型（info success warning error）（默认：warning）
- * 可选 delay 显示时长（默认：5000 ms）
+ * @param type 消息类型 info、success、warning、error（默认：warning）
+ * @param delay 显示时长（默认：5000 ms）
  */
 tool.toast = function (text) {
     tool.toastInstance = tool.toastInstance || new Ext.create('plugin.toast');
@@ -29,20 +30,9 @@ tool.toast = function (text) {
     tool.toastInstance[type](text);
 };
 
-//读取Cookie
-tool.getCookie = function (name) {
-    var defaultValue = arguments[1]; //默认值
-    tool.cookies = tool.cookies || Ext.util.Cookies;
-    var val = tool.cookies.get(name);
-    if (typeof (defaultValue) !== 'undefined' && defaultValue != null) {
-        val = val ? val : defaultValue;
-        tool.cookies.set(name, val, null, location.pathname);
-    }
-    return val;
-};
-
 /**
- * Ajax请求
+ * Ajax 请求
+ *
  * @param method
  * @param url
  * @param params
@@ -53,9 +43,7 @@ tool.ajax = function (method, url, params, callback) {
         method: method,
         url: url,
         params: params,
-        headers: {
-            'X-XSRF-TOKEN': tool.getCookie('XSRF-TOKEN')
-        },
+        headers: {'X-XSRF-TOKEN': Ext.util.Cookies.get('XSRF-TOKEN')},
         success: function (rsp) {
             try {
                 rsp = Ext.JSON.decode(rsp.responseText);
@@ -65,7 +53,7 @@ tool.ajax = function (method, url, params, callback) {
             callback(rsp);
         },
         failure: function (rsp) {
-            tool.toast('请求失败（状态码：' + rsp.status + '）');
+            tool.toast('Request failed (code：' + rsp.status + ')');
         }
     });
 };
