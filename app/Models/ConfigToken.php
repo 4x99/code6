2a712d08a87dd6912d98e5846bc1339f0d9b63db
huangyongxin92 <361661698@qq.com>
2020-05-06 11:56:55 +0800

    其他：ConfigToken 代码优化
    - 编辑接口优化
    - 前端代码和样式优化

--- app/Models/ConfigToken.php
+++ app/Models/ConfigToken.php
@@ -6,7 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class ConfigToken extends Model
 {
-    const UPDATED_AT = null;
     protected $table = 'config_token';
     protected $fillable = ['token', 'api_limit', 'description'];
 }
