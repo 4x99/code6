97edc925743a1944fcfa9e7b9f090af18e1518ef
GallopYD <zhangyida07@gmail.com>
2020-05-07 11:21:08 +0800

    重构：列表新增、删除优化

--- resources/views/configJob/index.blade.php
+++ resources/views/configJob/index.blade.php
@@ -16,19 +16,140 @@
                 }
             });
 
+            var btnDel = Ext.create('Ext.button.Button', {
+                text: '删除',
+                iconCls: 'icon-cross',
+                handler: function () {
+                    var grid = this.up('gridpanel');
+                    var selection = grid.selection;
+                    if (selection) {
+                        Ext.Msg.show({
+                            title: '警告',
+                            iconCls: 'icon-warning',
+                            message: '确定删除该行？',
+                            buttons: Ext.Msg.YESNO,
+                            icon: Ext.Msg.QUESTION,
+                            fn: function (btn) {
+                                if (btn === 'yes') {
+                                    tool.ajax('DELETE', '/api/configJob/' + selection.id, {}, function (data) {
+                                        if (data.success) {
+                                            tool.toast('删除成功！', 'success');
+                                            grid.store.reload();
+                                        } else {
+                                            tool.toast(data.message ?? '', 'error');
+                                        }
+                                    });
+                                }
+                            }
+                        });
+                    }
+                },
+            });
+
+            var btnAdd = Ext.create('Ext.button.Button', {
+                text: '新增',
+                iconCls: 'icon-add',
+                margin: '0 20 0 0',
+                handler: function () {
+                    Ext.create('Ext.window.Window', {
+                        title: '新增任务配置',
+                        width: 300,
+                        modal: true,
+                        layout: 'fit',
+                        items: [
+                            {
+                                xtype: 'form',
+                                layout: 'form',
+                                bodyPadding: 10,
+                                border: false,
+                                defaults: {
+                                    xtype: 'textfield',
+                                    allowBlank: false,
+                                },
+                                items: [
+                                    {
+                                        name: 'keyword',
+                                        fieldLabel: '关键词',
+                                    },
+                                    {
+                                        name: 'scan_page',
+                                        fieldLabel: '扫描页码',
+                                    },
+                                    {
+                                        name: 'scan_interval_min',
+                                        fieldLabel: '扫描间隔',
+                                    },
+                                    {
+                                        name: 'description',
+                                        fieldLabel: '描述',
+                                    },
+                                ],
+                                buttons: [
+                                    {
+                                        text: '重置',
+                                        handler: function () {
+                                            this.up('form').getForm().reset();
+                                        }
+                                    },
+                                    {
+                                        text: '提交',
+                                        formBind: true,
+                                        handler: function (btn) {
+                                            var params = this.up('form').getValues();
+                                            tool.ajax('POST', '/api/configJob', params, function (data) {
+                                                    if (data.success) {
+                                                        btn.up('window').close();
+                                                        tool.toast('新增成功！', 'success');
+                                                        grid.store.reload();
+                                                    } else {
+                                                        tool.toast(data.message ?? '', 'error');
+                                                    }
+                                                }
+                                            );
+                                        }
+                                    }
+                                ]
+                            }
+                        ]
+                    }).show()
+                }
+            });
+
+            var rowEditing = Ext.create('Ext.grid.plugin.RowEditing', {
+                pluginId: 'plnRowEditing',
+                errorSummary: false, //右侧提示
+                saveBtnText: '保存',
+                cancelBtnText: '取消',
+                listeners: {
+                    edit: function (editor, e) {
+                        var record = e.newValues;
+                        var params = {
+                            'keyword': record.keyword,
+                            'scan_page': record.scan_page,
+                            'scan_interval_min': record.scan_interval_min,
+                            'description': record.description,
+                        };
+                        tool.ajax('PUT', '/api/configJob/' + record.id, params, function (data) {
+                            if (data.success) {
+                                tool.toast('修改成功！', 'success');
+                                grid.store.reload();
+                            } else {
+                                tool.toast(data.message ?? '', 'error');
+                            }
+                        });
+                    }
+                }
+            });
+
             var grid = Ext.create('plugin.grid', {
                 store: store,
                 viewConfig: {
-                    stripeRows: false
+                    stripeRows: false,
                 },
                 tbar: [
                     '->',
-                    {
-                        text: '新增',
-                        margin: '0 30 0 0',
-                        iconCls: 'icon-add',
-                        handler: winAdd
-                    }
+                    btnDel,
+                    btnAdd,
                 ],
                 columns: [
                     {
@@ -91,72 +212,9 @@
                         dataIndex: 'updated_at',
                         flex: 1,
                     },
-                    {
-                        text: '操作',
-                        sortable: false,
-                        width: 80,
-                        align: 'center',
-                        xtype: 'widgetcolumn',
-                        widget: {
-                            xtype: 'buttongroup',
-                            baseCls: 'border:0',
-                            items: [
-                                {
-                                    text: '删除',
-                                    iconCls: 'icon-cross',
-                                    handler: function (obj) {
-                                        Ext.Msg.show({
-                                            title: '警告',
-                                            iconCls: 'icon-warning',
-                                            message: '确定删除该项？',
-                                            buttons: Ext.Msg.YESNO,
-                                            icon: Ext.Msg.QUESTION,
-                                            fn: function (btn) {
-                                                if (btn === 'yes') {
-                                                    var record = obj.up().getWidgetRecord();
-                                                    tool.ajax('DELETE', '/api/configJob/' + record.id, {}, function (data) {
-                                                        if (data.success) {
-                                                            tool.toast('删除成功！', 'success');
-                                                            grid.store.remove(record);
-                                                        } else {
-                                                            tool.toast(data.message ?? '', 'error');
-                                                        }
-                                                    });
-                                                }
-                                            }
-                                        });
-                                    }
-                                }
-                            ]
-                        }
-                    }
                 ],
                 plugins: [
-                    Ext.create('Ext.grid.plugin.RowEditing', {
-                        pluginId: 'plnRowEditing',
-                        errorSummary: false, //右侧提示
-                        saveBtnText: '保存',
-                        cancelBtnText: '取消',
-                        listeners: {
-                            edit: function (editor, e) {
-                                var record = e.newValues;
-                                var params = {
-                                    'keyword': record.keyword,
-                                    'scan_page': record.scan_page,
-                                    'scan_interval_min': record.scan_interval_min,
-                                    'description': record.description,
-                                };
-                                tool.ajax('PUT', '/api/configJob/' + record.id, params, function (data) {
-                                    if (data.success) {
-                                        tool.toast('修改成功！', 'success');
-                                        grid.store.reload();
-                                    } else {
-                                        tool.toast(data.message ?? '', 'error');
-                                    }
-                                });
-                            }
-                        }
-                    })
+                    rowEditing,
                 ],
             });
 
@@ -164,72 +222,9 @@
                 renderTo: Ext.getBody(),
                 height: '100%',
                 layout: 'fit',
-                items: [grid]
+                items: [grid],
             });
 
-            function winAdd() {
-                var win = Ext.create('Ext.window.Window', {
-                    title: '新增任务配置',
-                    width: 300,
-                    modal: true,
-                    layout: 'fit',
-                    items: [
-                        {
-                            xtype: 'form',
-                            layout: 'form',
-                            bodyPadding: 10,
-                            border: false,
-                            defaults: {
-                                xtype: 'textfield',
-                                allowBlank: false,
-                            },
-                            items: [
-                                {
-                                    name: 'keyword',
-                                    fieldLabel: '关键词',
-                                },
-                                {
-                                    name: 'scan_page',
-                                    fieldLabel: '扫描页码',
-                                },
-                                {
-                                    name: 'scan_interval_min',
-                                    fieldLabel: '扫描间隔',
-                                },
-                                {
-                                    name: 'description',
-                                    fieldLabel: '描述',
-                                },
-                            ],
-                            buttons: [
-                                {
-                                    text: '重置',
-                                    handler: function () {
-                                        this.up('form').getForm().reset();
-                                    }
-                                },
-                                {
-                                    text: '提交',
-                                    formBind: true,
-                                    handler: function () {
-                                        var params = this.up('form').getValues();
-                                        tool.ajax('POST', '/api/configJob', params, function (data) {
-                                                if (data.success) {
-                                                    win.close();
-                                                    tool.toast('提交成功！', 'success');
-                                                    grid.store.reload();
-                                                } else {
-                                                    tool.toast(data.message ?? '', 'error');
-                                                }
-                                            }
-                                        );
-                                    }
-                                }
-                            ]
-                        }
-                    ]
-                }).show();
-            }
         });
     </script>
 @endsection
