1637a857e8689dc9b7dee229e3e95b8bce58409f
GallopYD <18859669020@163.com>
2021-06-01 16:13:45 +0800

    特性：代理配置

---
+++ app/Http/Controllers/ConfigProxyController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ConfigCommon;
+use App\Services\GitHubService;
+use Illuminate\Http\Request;
+use Github\Client;
+use Github\HttpClient\Builder;
+use Http\Adapter\Guzzle6\Client as GuzzleClient;
+
+class ConfigProxyController extends Controller
+{
+    /**
+     * 代理配置
+     *
+     * @return array
+     */
+    public function index()
+    {
+        try {
+            return ['success' => true, 'data' => ConfigCommon::where('key', ConfigCommon::KEY_PROXY)->value('value')];
+        } catch (\Exception $e) {
+            return ['success' => false, 'message' => $e->getMessage()];
+        }
+    }
+
+    /**
+     * 更新代理配置
+     *
+     * @param  Request  $request
+     * @return array
+     */
+    public function store(Request $request)
+    {
+        try {
+            ConfigCommon::updateOrCreate(['key' => ConfigCommon::KEY_PROXY], $request->all('value'));
+            return ['success' => true];
+        } catch (\Exception $e) {
+            return ['success' => false, 'message' => $e->getMessage()];
+        }
+    }
+
+    /**
+     * 代理测试
+     *
+     * @param  Request  $request
+     * @return array
+     */
+    public function test(Request $request)
+    {
+        try {
+            $builder = new Builder(GuzzleClient::createWithConfig([
+                'timeout' => GitHubService::HTTP_TIMEOUT,
+                'proxy' => $request->input('value'),
+            ]));
+            $client = new Client($builder, 'v3.text-match');
+            $client->api('repo')->releases()->latest('4x99', 'code6');
+            return ['success' => true];
+        } catch (\Exception $e) {
+            return ['success' => false, 'message' => $e->getMessage()];
+        }
+    }
+}
