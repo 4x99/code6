0488ad84fb0b86603b10fac879d5570d909a1009
GallopYD <zhangyida07@foxmail.com>
2023-03-02 10:06:47 +0800

    修复：docker 部署 - 配置异常修复

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,11 +5,11 @@ cp -i .env.example .env
 chmod -R 755 storage
 chown -R www-data:www-data storage
 php artisan key:generate
-sed -i "s!DB_HOST=127.0.0.1!DB_HOST=$MYSQL_HOST!" .env
-sed -i "s!DB_PORT=3306!DB_PORT=$MYSQL_PORT!" .env
-sed -i "s!DB_DATABASE=code6!DB_DATABASE=$MYSQL_DATABASE!" .env
-sed -i "s!DB_USERNAME=!DB_USERNAME=$MYSQL_USERNAME!" .env
-sed -i "s!DB_PASSWORD=!DB_PASSWORD=$MYSQL_PASSWORD!" .env
+sed -i "s!DB_HOST=127.0.0.1.*!DB_HOST=$MYSQL_HOST!" .env
+sed -i "s!DB_PORT=3306.*!DB_PORT=$MYSQL_PORT!" .env
+sed -i "s!DB_DATABASE=code6.*!DB_DATABASE=$MYSQL_DATABASE!" .env
+sed -i "s!DB_USERNAME=.*!DB_USERNAME=$MYSQL_USERNAME!" .env
+sed -i "s!DB_PASSWORD=.*!DB_PASSWORD=$MYSQL_PASSWORD!" .env
 php artisan migrate --force
 
 # 配置任务调度
